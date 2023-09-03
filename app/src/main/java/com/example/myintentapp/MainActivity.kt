dab1d9bac4371111023d7897c9282520f302da40
Rifky Al Mukmin Rachmat <rifkyalmukmin@gmail.comgit config --global user.name rifkyalmukmin@gmail.com>
2023-09-03 20:22:16 +0700

    first

---
+++ app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myintentapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity_data)
+
+    }
+}
